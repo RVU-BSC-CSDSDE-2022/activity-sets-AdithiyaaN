1f6e9af45b2c9c75b0942923ebbe4f09a1b243b7
Adithiyaa N <114280252+AdithiyaaN@users.noreply.github.com>
2022-12-31 16:23:02 +0000

    Done!

--- set02/problem06.c
+++ set02/problem06.c
@@ -1,48 +1,38 @@
 // Write a program to reverse a string.
 
+#include <stdio.h>
 void input_string(char *a);
 void str_reverse(char *str, char *rev_str);
 void output(char *a, char *reverse_a);
-
-void main()
-{
-  int x,str,rev_str,a,reverse_a;
-  input_string(x);
-  str_reverse(str,rev_str);
-  output(a,reverse_a);
+int main(void) {
+  char str[100], rev_str[100];
+  input_string(str);
+  str_reverse(str, rev_str);
+  output(str, rev_str);
 }
 
-void input_string(char *a)
-{
-  char x[100];
+void input_string(char *a) {
   printf("Enter a string: ");
-  for(int i=0;i<100;i++)
-  {
-    if(i!="\0")
-    {scanf("%s",x[i]);}
-  } 
+  scanf("%s", a);
 }
 
-void str_reverse(char *str, char *rev_str)
-{
-  char str[100];
-  int n;
-  for (int i=0;i<100;i++)
-    {
-      if(str[i]!="\0")
-      {n++;}
-        }
-  for(int i = 0;i<100;i++)
-    {
-      if(i!="\0")
-      // {*rev_str = *str;}
-      {
-        *rev_str = str[(n-1)-i];
-      }
-    }
+void str_reverse(char *str, char *rev_str) {
+  int len = 0;
+  char q;
+  for (int i = 0; str[i]; i++) {
+    len = len + 1;
+  }
+
+  for (int i = 0; i < len / 2; i++) {
+    q = str[i];
+    str[i] = str[(len - 1) - i];
+    str[(len - 1) - i] = q;
+  }
+  for (int i = 0; i <= len; i++) {
+    rev_str[i] = str[i];
+  }
 }
 
-void output(char *a, char *reverse_a)
-{
-    {printf(reverse_a,a);}
+void output(char *a, char *reverse_a) {
+  { printf("%s\n", reverse_a); }
 }
