fb673167ef65e045a3bde2588ba0a7f76a082236
Adithiyaa N <114280252+AdithiyaaN@users.noreply.github.com>
2023-01-04 02:29:42 +0000

    done!

---
+++ set03/problem04.c
@@ -0,0 +1,39 @@
+// 4. Write a program to find nth number in fibonacci sequence.
+
+#include<stdio.h>
+int input();
+int find_fibo(int n);
+void output(int n, int fibo);
+
+int main(void)
+{
+  int n,fibo;
+  n = input();
+  fibo = find_fibo(n);
+  output(n,fibo);
+}
+
+int input()
+{
+  int f;
+  printf("Enter any number: ");
+  scanf("%d",&f);
+  return f;
+}
+
+int find_fibo(int n)
+{
+  int a[n],f;
+      a[0] = 0;
+      a[1] = 1;
+      a[2] = 1;
+  // Formula f(n) = f(n-1) + f(n-2) where n is a iteration veriable
+  for(int i=2; i <= n ;i++)
+    {a[i] = a[i-1] + a[i-2];}
+  return a[n];
+}
+
+void output(int n, int fibo)
+{
+  printf("fino(%d) = %d \n",n,fibo);
+}
