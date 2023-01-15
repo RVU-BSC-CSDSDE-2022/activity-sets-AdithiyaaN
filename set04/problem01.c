6693313c7fb41873253f8a43d89b19726d1a9264
Adithiyaa N <114280252+AdithiyaaN@users.noreply.github.com>
2023-01-15 15:01:57 +0000

    Done!

---
+++ set04/problem01.c
@@ -0,0 +1,43 @@
+// 1. Write a program to find sum of two fractions
+
+#include<stdio.h>
+
+void input(int *num1, int *den1, int *num2, int *den2);
+void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den);
+void output(int num1, int den1, int num2, int den2, int res_num, int res_den);
+
+int main(void)
+{
+  int num1,den1,num2,den2,res_num,res_den;
+  input(&num1,&den1,&num2,&den2);
+  add(num1,den1,num2,den2,&res_num,&res_den);
+  output(num1,den1,num2,den2,res_num,res_den);
+}
+
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  printf("Enter first fraction: ");
+  scanf("%d %d",num1,den1);
+
+  printf("Enter second fraction: ");
+  scanf("%d %d",num2,den2);
+}
+
+void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den)
+{
+  *res_num = num1 + num2;
+  if (den1 != den2)
+  {
+    *res_den = den1 * den2;
+    *res_num = num1 * den2 + num2 * den1;
+  }
+  else
+  {
+    *res_den = den1;
+  }
+}
+
+void output(int num1, int den1, int num2, int den2, int res_num, int res_den)
+{
+  (res_den != den1)? printf("%d/%d + %d/%d = %d/%d\n",num1,den1,num2,den2,res_num,res_den) : printf("%d/%d + %d/%d = %d\n",num1,den1,num2,den2,res_num);
+}
